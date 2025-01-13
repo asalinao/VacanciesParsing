584c83b3347ae5580fa25d96e14ae524f5726a3e
asalinao <solofunch@gmail.com>
2025-01-13 15:23:55 +0300

    clickhouse

--- uploadToGoogleSheets.py
+++ uploadToGoogleSheets.py
@@ -12,8 +12,8 @@ query = "SELECT * FROM vacancies"
 df = client.query_df(query)
 
 df = df.fillna('')
-print(df.info())
-for column in df.select_dtypes(include=[np.datetime64, 'datetime']):
+
+for column in df.select_dtypes(include=[np.datetime64, 'datetime64']):
     df[column] = df[column].dt.strftime('%Y-%m-%d %H:%M:%S')
 
 creds = ServiceAccountCredentials.from_json_keyfile_name("hhvacancy-04d0d07b0d5b.json", scope)
