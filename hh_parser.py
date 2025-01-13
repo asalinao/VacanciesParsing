09d2bfc636391051a60d4d96cf38a46c4ee560d0
asalinao <solofunch@gmail.com>
2025-01-13 14:06:25 +0300

    clickhouse

--- hh_parser.py
+++ hh_parser.py
@@ -61,7 +61,7 @@ for query_string in QUERIES:
     for item in vacancies_from_response:
         for vacancy in item:
             vacancy_id = vacancy['id']
-            if client.query(f"SELECT count(*) FROM headhunter.vacancies WHERE vacancy_id={vacancy['id']}")[0][0] > 0:
+            if client.query(f"SELECT count(*) FROM vacancies WHERE vacancy_id={vacancy['id']}")[0][0] > 0:
                 continue
             name = vacancy['name'].replace("'", "").replace('"', '')
             has_test = int(vacancy['has_test'])
@@ -133,5 +133,5 @@ for query_string in QUERIES:
                 pos_level
             ]
 
-            client.insert('headhunter.vacancies', new_vacancy, column_names=COLUMNS)
+            client.insert('vacancies', new_vacancy, column_names=COLUMNS)
 client.close()
